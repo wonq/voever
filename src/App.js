7376eb87ccc7b72dd81e74ef578944b4b7a00b82
Iwan Nusirwan <wonkinter@gmail.com>
2020-08-02 07:46:42 +0700

    App: Create logo & contents hero

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+
+import Logo from "./images/logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>yo...</p>
-      </header>
+      <div className="App-header">
+        <img src={Logo} />
+        <h2>coming up</h2>
+        <p>
+          Hold tight as we get our working robots together and produce the most
+          astonishing product ever.
+        </p>
+      </div>
     </div>
   );
 }
