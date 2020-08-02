c6abaceaa94313c9fff9a4b060d660133700da6b
Iwan Nusirwan <wonkinter@gmail.com>
2020-08-02 08:53:11 +0700

    Import icon & Add Contacts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import Logo from "./images/logo.svg";
+import logo from "./images/logo.svg";
+import instagram from "./images/icon-instagram.svg";
+import youtube from "./images/icon-youtube.svg";
+import tiktok from "./images/icon-tiktok.svg";
+import phone from "./images/icon-phone.svg";
+import email from "./images/icon-email.svg";
 import "./App.css";
 
 function App() {
@@ -8,7 +13,7 @@ function App() {
     <div className="hero">
       <div className="container">
         <span className="logo">
-          <img src={Logo} alt="voever.com" />
+          <img src={logo} alt="voever.com" />
         </span>
         <div className="content">
           <h2 className="tagline">coming up</h2>
@@ -17,7 +22,23 @@ function App() {
             most astonishing product ever.
           </p>
         </div>
-        <ul>Social Media</ul>
+        <ul className="contacts">
+          <li>
+            <img src={instagram} alt="instagram" />
+          </li>
+          <li>
+            <img src={youtube} alt="youtube" />
+          </li>
+          <li>
+            <img src={tiktok} alt="tiktok" />
+          </li>
+          <li>
+            <img src={phone} alt="phone" />
+          </li>
+          <li>
+            <img src={email} alt="email" />
+          </li>
+        </ul>
       </div>
     </div>
   );
