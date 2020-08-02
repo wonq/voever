8d306ee36b66e94a139d7a1b9276c7ab0c18f771
Iwan Nusirwan <wonkinter@gmail.com>
2020-08-02 08:39:59 +0700

    Update Markup & Style Container & Contents

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ function App() {
     <div className="hero">
       <div className="container">
         <img src={Logo} alt="voever.com" />
-        <h2 className="tagline">coming up</h2>
-        <p className="description">
-          Hold tight as we get our working robots together and produce the most
-          astonishing product ever.
-        </p>
+        <div className="content">
+          <h2 className="tagline">coming up</h2>
+          <p className="description">
+            Hold tight as we get our working robots together and produce the
+            most astonishing product ever.
+          </p>
+        </div>
+        <ul>Social Media</ul>
       </div>
     </div>
   );
